02ee0c3bd55ca527721fddb86014d6fafd8a5407
swaterisaac <isaac.rojas@usach.cl>
2019-10-10 19:59:25 -0300

    Hecho PREDICADOS para borrar elementos de lista, tanto con index como con el elemento

--- initial.pl
+++ initial.pl
@@ -7,20 +7,34 @@
 %L: Lista cualquiera
 %E: Número entero que representa el elemento que queremos extraer de una lista
 %R: El elemento extraído.
+
+%Lrel: Lista resultante después de haber hecho la operación correspondiente.
 %Predicados
 %scene(ID,M,N,Scene).
 %get(L,N,R).
-
+%deleteL(L,R,Lrel).
+%deleteL2(L,N,Lrel).
 
 %hechos
-
+get([],_,[]).
 get([X|_],0,X):-!.
+get([_|Xs],N,R):- Nsig is N-1, get(Xs,Nsig,R).
 
+deleteL([X],X,[]):-!.
+deleteL([X|Xs],X,Xs):-!.
+deleteL([Y|Xs],X,[Y|Xs2]):- deleteL(Xs,X,Xs2).
+
+
+deleteL2([],0,[]):-!.
+deleteL2([_|Xs],0,Xs):-!.
+deleteL2(L,N,R):- get(L,N,E),deleteL(L,E,R).
 
 
 
 %Reglas
-get([_|Xs],N,R):- Nsig is N-1, get(Xs,Nsig,R).
+
+
+
 
 
 

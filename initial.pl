9a64283d049e1832129a58619851cbec374de64a
swaterisaac <isaac.rojas@usach.cl>
2019-10-10 18:59:06 -0300

    Hecha la función get para obtener un elemento N de una lista

--- initial.pl
+++ initial.pl
@@ -1,12 +1,26 @@
-%base de datos
+%Dominio
+%ID: Número entero positivo que indica una identificación de un escenario. No pueden haber 2 ID iguales.
+%M: Número entero positivo que representa la cantidad de suelo que hay en un escenario.
+%N: Número entero positivo que representa la altura que posee un escenario.
+%Scene: Lista
+
+%L: Lista cualquiera
+%E: Número entero que representa el elemento que queremos extraer de una lista
+%R: El elemento extraído.
+%Predicados
+%scene(ID,M,N,Scene).
+%get(L,N,R).
+
 
 %hechos
 
-pertenece(escena,gusano).
-pertenece(escena,bala).
-pertenece(gusano,bala).
-pertenece(vacio,bala).
+get([X|_],0,X):-!.
 
+
+
+
+%Reglas
+get([_|Xs],N,R):- Nsig is N-1, get(Xs,Nsig,R).
 
 
 

862ab757f0d5a63750ff693b01351b0106734718
swaterisaac <isaac.rojas@usach.cl>
2019-10-10 22:40:34 -0300

    Creadas las escenas estáticas

--- initial.pl
+++ initial.pl
@@ -10,7 +10,7 @@
 
 %Lrel: Lista resultante después de haber hecho la operación correspondiente.
 %Predicados
-%scene(ID,M,N,Scene).
+%scene(ID,M,N,E,D,Scene).
 %get(L,N,R).
 %deleteL(L,R,Lrel).
 %deleteL2(L,N,Lrel).
@@ -29,7 +29,14 @@ deleteL2([],0,[]):-!.
 deleteL2([_|Xs],0,Xs):-!.
 deleteL2(L,N,R):- get(L,N,E),deleteL(L,E,R).
 
+scene(1,10,5,2,1,[10,5,playing,[1,1],[[9,1],[10,1]],[]]).
+scene(2,10,5,4,2,[10,5,playing,[1,1],[[7,1],[8,1],[9,1],[10,1]],[]]).
+scene(3,10,5,5,3,[10,5,playing,[1,1],[[6,1],[7,1],[8,1],[9,1],[10,1]],[]]).
+scene(4,12,10,4,1,[12,10,playing,[1,1],[[9,1],[10,1],[11,1],[12,1]],[]]).
+scene(5,12,10,6,2,[12,10,playing,[1,1],[[7,1],[8,1],[9,1],[10,1],[11,1],[12,1]],[]]).
+scene(6,20,20,8,3,[20,20,playing,[1,1],[[13,1],[14,1],[15,1],[16,1],[17,1],[18,1],[19,1],[20,1]],[]]).
 
+createScene(N,M,E,D,_,Scene):-scene(_,M,N,E,D,Scene).
 
 %Reglas
 

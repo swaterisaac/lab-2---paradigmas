79799017765924f8fb99a1583ca56ce51c0981f0
swaterisaac <isaac.rojas@usach.cl>
2019-10-15 20:12:58 -0300

    Cambiada la representación de los gusanos, completado el checkscene y hecho el move

--- initial.pl
+++ initial.pl
@@ -14,6 +14,7 @@
 %get(L,N,R).
 %deleteL(L,R,Lrel).
 %deleteL2(L,N,Lrel).
+%moveMember(SceneIn,Member,MoveDir,Seed,SceneOut)
 
 %hechos
 get([],_,[]).
@@ -29,29 +30,48 @@ deleteL2([],0,[]):-!.
 deleteL2([_|Xs],0,Xs):-!.
 deleteL2(L,N,R):- get(L,N,E),deleteL(L,E,R).
 
-%TDA Worm
-getPosX(Worm,X):- get(Worm,0,X).
-getPosY(Worm,Y):- get(Worm,1,Y).
 
 status(defeat).
 status(playing).
 status(win).
 
-scene(1,10,5,2,1,[10,5,playing,[1,1],[[9,1],[10,1]],[]]).
-scene(2,10,5,4,2,[10,5,playing,[1,1],[[7,1],[8,1],[9,1],[10,1]],[]]).
-scene(3,10,5,5,3,[10,5,playing,[1,1],[[6,1],[7,1],[8,1],[9,1],[10,1]],[]]).
-scene(4,12,10,4,1,[12,10,playing,[1,1],[[9,1],[10,1],[11,1],[12,1]],[]]).
-scene(5,12,10,6,2,[12,10,playing,[1,1],[[7,1],[8,1],[9,1],[10,1],[11,1],[12,1]],[]]).
-scene(6,20,20,8,3,[20,20,playing,[1,1],[[13,1],[14,1],[15,1],[16,1],[17,1],[18,1],[19,1],[20,1]],[]]).
+bullet([]).
+bullet([X,Y,_]):- X >= 1, Y >= 1.
+
+
+
+worm(X):- X >= 1.
+
+comprobatePlayer(_,[]).
+comprobatePlayer(Player,[X|Xs]):- not(Player == X), comprobatePlayer(Player,Xs).
+
+scene(1,10,5,2,1,[10,5,playing,1,[9,10],[]]).
+scene(2,10,5,4,2,[10,5,playing,1,[7,8,9,10],[]]).
+scene(3,10,5,5,3,[10,5,playing,1,[6,7,8,9,10],[]]).
+scene(4,12,10,4,1,[12,10,playing,1,[9,10,11,12],[]]).
+scene(5,12,10,6,2,[12,10,playing,1,[7,8,9,10,11,12],[]]).
+scene(6,20,20,8,3,[20,20,playing,1,[13,14,15,16,17,18,19,20],[]]).
 
 createScene(N,M,E,D,_,Scene):-scene(_,M,N,E,D,Scene).
 
 
 checkScene1(Scene):- get(Scene,0,M),get(Scene,1,N),get(Scene,2,Status),
-					get(Scene,3,Player),get(Scene,4,Enemies),get(Scene,5,Bullet),
-					M >= 10, N >= 5, status(Status).
+					get(Scene,3,Player),get(Scene,5,Bullet),
+					M >= 10, N >= 5, status(Status),bullet(Bullet),worm(Player),
+					Player =< M.
+					
+checkScene2(Scene):- get(Scene,3,Player), get(Scene,4,Enemies), comprobatePlayer(Player,Enemies).
+
+checkScene(Scene):- checkScene1(Scene),checkScene2(Scene).
+						
+						
 
 
+moveMember(SceneIn,_,MoveDir,_,SceneOut):- get(SceneIn,0,M),get(SceneIn,1,N),get(SceneIn,2,Status),
+											get(SceneIn,3,Player),get(SceneIn,4,Enemies),get(SceneIn,5,Bullet),
+											Xnew is Player + MoveDir,SceneOut = [M,N,Status,Xnew,Enemies,Bullet],
+											checkScene(SceneOut).
+											
 %Reglas
 
 
